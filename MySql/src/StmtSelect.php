c37b0844bce44135796fac20acaadde0810a9dd7
bdd88 <90480187+bdd88@users.noreply.github.com>
2022-09-07 13:00:55 -0500

    Added function support for SELECT statements. Added sorting for SELECT statements.

--- MySql/src/StmtSelect.php
+++ MySql/src/StmtSelect.php
@@ -6,20 +6,80 @@ class StmtSelect extends AbstractStmt
     use TraitWhereClause;
     
     private string $columnString;
+    private array $sorts;
+    private string $orderString;
+
+    /**
+     * Verify a MySql function is supported.
+     *
+     * @throws Exception If the function is not supported.
+     * @param string $functionName The function name to check for.
+     * @return string The supplied function name in uppercase.
+     */
+    private function verifyMySqlFunction(string $functionName): string
+    {
+        $functionName = strtoupper($functionName);
+        $supportedFunctions = array('AVG', 'COUNT', 'MAX', 'MIN', 'SUM');
+        if (!in_array($functionName, $supportedFunctions)) {
+            throw new Exception('MySql function is not supported. Supported functions: ' . implode(', ', $supportedFunctions));
+        }
+        return $functionName;
+    }
 
     /** Filter results to only include specified columns. If nothing is specified, all columns will be returned. */
-    public function columns(array $columnNames): StmtSelect
+    public function columns(...$columnNames): StmtSelect
     {
         $columnString = '';
         $lastKey = array_key_last($columnNames);
         foreach ($columnNames as $key => $columnName) {
-            $columnString .= $this->encapColumnString($columnName);
+            if (is_array($columnName)) {
+                $functionName = $this->verifyMySqlFunction(key($columnName));
+                $columnString .= $functionName . '(' . $this->encapColumnString(current($columnName)) . ')';
+            } else {
+                $columnString .= $this->encapColumnString($columnName);
+            }
             if ($key !== $lastKey) {
                 $columnString .= ', ';
             }
         }
         $this->columnString = $columnString;
         return $this;
+    }
+
+    /**
+     * Sort results by one or more columns.
+     *
+     * @param string|array $columnName Name of the column to sort.
+     * @param string $direction Either asc or desc
+     * @return StmtSelect
+     */
+    public function addSort(string|array $columnName, string $direction): StmtSelect
+    {
+        $direction = strtoupper($direction);
+        if ($direction !== 'ASC' && $direction !== 'DESC') {
+            throw new Exception('Sort direction must be either ASC or DESC.');
+        }
+
+        if (gettype($columnName) === 'string') {
+            $this->sorts[] = $this->encapColumnString($columnName) . ' ' . $direction; 
+        } else {
+            $functionName = $this->verifyMySqlFunction(key($columnName));
+            $this->sorts[] = $functionName . '(' . $this->encapColumnString(current($columnName)) . ') ' . $direction;
+        }
+        return $this;
+    }
+
+    public function constructOrderString(array $sorts): string
+    {
+        $orderString = 'ORDER BY ';
+        $lastKey = array_key_last($sorts);
+        foreach ($sorts as $key => $sortString) {
+            $orderString .= $sortString;
+            if ($key !== $lastKey) {
+                $orderString .= ', ';
+            }
+        }
+        return $orderString;
     }
 
     /**
@@ -37,6 +97,9 @@ class StmtSelect extends AbstractStmt
         }
         if (isset($this->matches)) {
             $queryString .= ' ' . $this->constructWhereString($this->matches);
+        }
+        if (isset($this->sorts)) {
+            $queryString .= ' ' . $this->constructOrderString($this->sorts);
         }
 
         // Run the query and return the results as an array of objects.
